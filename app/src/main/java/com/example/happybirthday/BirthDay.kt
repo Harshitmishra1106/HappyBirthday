a2653387f42624df8031e3131a5dc2084efbf935
Harshit <harshitmishragarhwa@gmail.com>
2024-07-06 21:19:16 +0530

    Primary Commit

---
+++ app/src/main/java/com/example/happybirthday/BirthDay.kt
@@ -0,0 +1,29 @@
+package com.example.happybirthday
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.happybirthday.databinding.ActivityBirthDayBinding
+import com.example.happybirthday.databinding.ActivityMainBinding
+
+class BirthDay : AppCompatActivity() {
+    private lateinit var binding: ActivityBirthDayBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityBirthDayBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val intent = intent
+        val str = intent.getStringExtra("To wish")
+        val str1 = intent.getStringExtra("Wisher")
+        binding.textView.text = str
+        binding.textView2.text = str1
+    }
+}
--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.happybirthday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happybirthday.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btn.setOnClickListener{
+            val wish = binding.wish.text.toString()
+            val wisher = binding.wisher.text.toString()
+            val intent = Intent(applicationContext,BirthDay::class.java)
+            intent.putExtra("To wish",wish)
+            intent.putExtra("Wisher",wisher)
+            startActivity(intent)
+        }
     }
 }
